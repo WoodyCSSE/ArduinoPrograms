9d3bb5137602740ebc59cc943fa8e3039e371adf
Woody Tilbury <02sherwood.tilbury@gulllakecs.org>
2022-02-28 09:05:34 -0800

    Add PWM to LED 10 and LED 9 Fading Is Stable

--- Fade/Fade.ino
+++ Fade/Fade.ino
@@ -16,21 +16,25 @@
 //Programmer: Woody Tilbury
 //Date:2.24.2022
 
-int led = 9;           // the PWM pin the LED is attached to
+int ledNINE = 9;       // the PWM pin the LED is attached to
+int ledTEN = 10;
 int brightness = 0;    // how bright the LED is
 int fadeAmount = 5;    // how many points to fade the LED by
 
 // the setup routine runs once when you press reset:
 void setup() {
   // declare pin 9 to be an output:
-  pinMode(led, OUTPUT);
+  pinMode(ledNINE, OUTPUT);
+    // declare pin 8 to be an output:
+  pinMode(ledTEN, OUTPUT);
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
   // set the brightness of pin 9:
   
- analogWrite(led, brightness);
+ analogWrite(ledNINE, brightness);
+ analogWrite(ledTEN, brightness);
 
   // change the brightness for next time through the loop:
   brightness = brightness + fadeAmount;
