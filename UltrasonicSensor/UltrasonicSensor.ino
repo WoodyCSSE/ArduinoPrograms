d499ef3d0bb0fa20440445772603a4828430c018
Woody Tilbury <02sherwood.tilbury@gulllakecs.org>
2022-03-03 09:09:26 -0800

    Updated Programming Info and Resource

--- UltrasonicSensor/UltrasonicSensor.ino
+++ UltrasonicSensor/UltrasonicSensor.ino
@@ -1,3 +1,8 @@
+
+//Programmer: Woody Tilbury
+//Date: 3.3.2022
+//Resource:https://create.arduino.cc/projecthub/knackminds/how-to-measure-distance-using-ultrasonic-sensor-hc-sr04-a-b9f7f8
+
 void setup() {
   // put your setup code here, to run once:
 
